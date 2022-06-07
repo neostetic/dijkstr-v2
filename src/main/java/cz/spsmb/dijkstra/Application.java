c00a9864bd7fb00a1b9a7a71a2d61c9b8b8892e5
Jan Poláček <gg.polacek@gmail.com>
2022-06-07 11:21:14 +0200

    Update and Fixed Graph with Double Change Node

--- src/main/java/cz/spsmb/dijkstra/Application.java
+++ src/main/java/cz/spsmb/dijkstra/Application.java
@@ -2,6 +2,7 @@ package cz.spsmb.dijkstra;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,6 +12,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image(Application.class.getResourceAsStream("icon.png")));
         stage.setTitle("Dijsktra's Application");
         stage.setResizable(false);
         stage.setScene(scene);
